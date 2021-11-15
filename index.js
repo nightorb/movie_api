a49e3efe836ace4c47e35c7a80dc608d2f761f9a
nightorb <ana.nath@gmx.de>
2021-11-15 01:30:18 +0100

    include input validation

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ require('./passport');
 
 app.use(passport.initialize());
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin)
+    return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn\'t allow access from orgigin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app);
+
+const { check, validationResult } = require('express-validator');
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
@@ -134,31 +151,46 @@ app.get('/actors/:name', passport.authenticate('jwt', { session: false }), (req,
 
 
 // add a user (register)
-app.post('/register', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + ' already exists');
-      } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-        })
-        .then((user) => {
-          res.status(201).json(user)
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send('Error: ' + err);
-        })
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+app.post('/register', passport.authenticate('jwt', { session: false }),
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username can only contain alphanumeric characters, "-" and "_".').isAlphanumeric().isWhitelisted('-', '_'),
+    check('Password', 'Password is required').notEmpty().isStrongPassword(),
+    check('Email', 'Email does not appear to be valid').isEmail().normalizeEmail()
+  ],
+  (req, res) => {
+
+    // check validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: error.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => {
+            res.status(201).json(user)
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+          })
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
 });
 
 // get a user by username
@@ -263,6 +295,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on port ' + port);
 });
